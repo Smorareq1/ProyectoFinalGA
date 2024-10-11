43ec7a8efd9bda5209e4126a0543dab36e07b246
Smorareq1 <smorareq1@gmail.com>
2024-10-10 21:51:15 -0600

    Refactor y renombres

--- src/main/java/Clases/Main.java
+++ src/main/java/Clases/Main.java
@@ -7,12 +7,12 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/fxml/ventanaPrincipal.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/fxml/ventanaPrincipal.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        stage.setTitle("ProyectoFinal Gestion-De-Archivos!");
         stage.setScene(scene);
         stage.show();
     }
