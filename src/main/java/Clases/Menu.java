a5e1740bb2bd4004e89e6362a405e4e7b69cec42
Smorareq1 <smorareq1@gmail.com>
2024-10-11 21:10:29 -0600

    Menu

--- src/main/java/Clases/Menu.java
+++ src/main/java/Clases/Menu.java
@@ -7,13 +7,5 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 
-public class Menu extends Application {
-    @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Menu.class.getResource("/fxml/ventanaPrincipal.fxml"));
-        Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("ProyectoFinal Gestion-De-Archivos!");
-        stage.setScene(scene);
-        stage.show();
-    }
-}
+
+
--- src/main/java/Controllers/MenuController.java
+++ src/main/java/Controllers/MenuController.java
@@ -1,11 +1,26 @@
 package Controllers;
 
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class MenuController {
+
+    @FXML
+    private ImageView exitImage;
+
+    @FXML
+    public void initialize() {
+        // Configurar el evento de clic para el ImageView
+        exitImage.setOnMouseClicked((MouseEvent event) -> {
+            Platform.exit(); // Cierra completamente el programa
+        });
+    }
 }
