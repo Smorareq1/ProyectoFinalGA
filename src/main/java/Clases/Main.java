0a4270a4cd3ee81c0461657b2c8f8f6497046b88
SusanaG17 <susanapaola@gmail.com>
2024-11-09 20:14:37 -0600

    Agregar linea a las marcas de carros y pasarlas a json. Editar y eliminar.

--- src/main/java/Clases/Main.java
+++ src/main/java/Clases/Main.java
@@ -21,6 +21,7 @@ public class Main extends Application {
         GestorDeArchivos.cargarVehiculosDesdeJson();
 
 
+
         //Una vez cargados los vehiculos, cargar sets
         GestorDeArchivos.cargarSets();
 
